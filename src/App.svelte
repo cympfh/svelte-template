8e21e2dc7c7f9258f35c96ca7111801cbefaa65d
cympfh <cympfh@gmail.com>
2021-07-13 18:07:06 +0900

    indent with spaces

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,25 @@
 <script>
-	import Icon from "svelte-awesome";
-	import { camera } from "svelte-awesome/icons";
-	import Footer from "./components/Footer.svelte";
+    import Icon from "svelte-awesome";
+    import { camera } from "svelte-awesome/icons";
+    import Footer from "./components/Footer.svelte";
 </script>
 
 <section class="hero">
-	<div class="hero-body">
-		<p class="title">Hello World!</p>
-		<p class="subtitle">Svelte Sample</p>
-	</div>
+    <div class="hero-body">
+        <p class="title">Hello World!</p>
+        <p class="subtitle">Svelte Sample</p>
+    </div>
 </section>
 <div class="section">
-	<div class="container">
-		<div>
-			<Icon data={camera} scale="1" />
-			Body Text.
-		</div>
-	</div>
+    <div class="container">
+        <div>
+            <Icon data={camera} scale="1" />
+            Body Text.
+        </div>
+    </div>
 </div>
 <Footer name="@cympfh" />
 
 <style global lang="scss">
-	@import "main.scss";
+    @import "main.scss";
 </style>
